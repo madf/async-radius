48eaa18c4ab5b9b45ecb4c90217b6f146b4c231a
Elena Mamontova <helenh463@gmail.com>
2021-11-18 15:22:37 +0200

    Initial. The parameter names in comments removed in the handle_receive(), handle_send() functions.

--- src/server.h
+++ src/server.h
@@ -11,8 +11,8 @@ class Server
 
     private:
         void start_receive();
-        void handle_receive(const boost::system::error_code& error, std::size_t /*bytes_transferred*/);
-        void handle_send(std::string /*message*/, const boost::system::error_code& /*error*/, std::size_t /*bytes_transferred*/);
+        void handle_receive(const boost::system::error_code& error, std::size_t);
+        void handle_send(std::string, const boost::system::error_code&, std::size_t);
 
         boost::asio::ip::udp::socket socket_;
         boost::asio::ip::udp::endpoint remote_endpoint_;
