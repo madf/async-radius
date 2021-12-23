77d1151af2378ccb3249608a5f41e17c104f1b7c
Elena Mamontova <helenh463@gmail.com>
2021-12-23 16:46:58 +0200

    Initial. File name changed from packets.h to packet_codes.h. The enum Attribute_type removed. Formatting fixed.

---
+++ src/packet_codes.h
@@ -0,0 +1,16 @@
+#ifndef PACKET_CODES_H
+#define PACKET_CODES_H
+
+enum Packet_Codes
+{
+    ACCESS_REQUEST = 1,
+    ACCESS_ACCEPT = 2,
+    ACCESS_REJECT = 3,
+    ACCOUNTING_REQUEST = 4,
+    ACCOUNTING_RESPONSE = 5,
+    ACCESS_CHALLENGE = 11,
+    STATUS_SERVER = 12,
+    STATUS_CLIENT = 13
+};
+
+#endif
