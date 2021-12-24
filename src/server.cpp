283c94f15818aa54abd5a4c823d1fc9b41217fa4
Elena Mamontova <helenh463@gmail.com>
2021-12-24 13:05:57 +0200

    Initial. The header files "packet.h", <iostream> removed, header files "packet_codes.h", "attribute_types.h", <openssl/md5.h> added. The variable send_length2 definition changed. Maximum value of cycle variable fixed for copying request authenticator in send_buffer. The variable std::string message removed. The unsigned char md buffer added for writting hash. Added function call MD5(). Added cycle for writing to send_buffer response authenticator.

--- src/server.cpp
+++ src/server.cpp
@@ -1,8 +1,9 @@
 #include "server.h"
-#include "packets.h"
+#include "packet_codes.h"
+#include "attribute_types.h"
 #include <functional> //std::bind
 #include <string>
-#include <iostream>
+#include <openssl/md5.h>
 
 using boost::asio::ip::udp;
 using boost::system::error_code;
@@ -33,32 +34,38 @@ void Server::handle_receive(const error_code& error, std::size_t bytes)
             if (recv_buffer_[0] == ACCESS_REQUEST)
                 send_code = 2;
 
-            std::string secr("secret");
-            uint8_t send_length1 = 0;
-            uint8_t send_length2 = 20 + secr.length();
             std::array<uint8_t, 4096> send_buffer;
+            uint8_t send_length1 = 0;
+            uint8_t send_length2 = 20;
 
             send_buffer[0] = send_code;
             send_buffer[1] = recv_buffer_[1];
             send_buffer[2] = send_length1;
             send_buffer[3] = send_length2;
 
-            for (int i =0; i <= 15; i++)
-                send_buffer[i + 4] = recv_buffer_[i + 4];
+            for (int i = 4; i <= 19; i++)
+                send_buffer[i] = recv_buffer_[i];
 
-            for (int i =0; i <= 15; i++)
+            std::string secr("secret");
+
+            for (int i = 0; i <= 5; i++)
                 send_buffer[i + 20] = secr[i];
 
-            const std::string message("Hello, World!");
+            unsigned char md[MD5_DIGEST_LENGTH];
 
-            socket_.async_send_to(boost::asio::buffer(message), remote_endpoint_,
-              std::bind(&Server::handle_send, this, message, pls::_1, pls::_2));
+            MD5(send_buffer.data(), send_length2 + secr.length(), md);
+
+            for (int i = 0; i <= 15; i++)
+                send_buffer[i + 4] = md[i];
+
+          socket_.async_send_to(boost::asio::buffer(send_buffer), remote_endpoint_,
+              std::bind(&Server::handle_send, this, pls::_1, pls::_2));
 
             start_receive();
         }
     }
 }
 
-void Server::handle_send(std::string /*message*/, const error_code& /*error*/, std::size_t /*bytes_transferred*/)
+void Server::handle_send(const error_code& /*error*/, std::size_t /*bytes_transferred*/)
 {
 }
