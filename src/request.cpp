51dff64c3879eea1c6a3a7a73bc66e747de63303
Elena Mamontova <helenh463@gmail.com>
2022-02-10 15:43:43 +0200

    Initial. The new file request.cpp added.

---
+++ src/request.cpp
@@ -0,0 +1,26 @@
+#include "request.h"
+
+Request::Request(const std::array<uint8_t, 4096>& m_recvBuffer)
+{
+    m_type = m_recvBuffer[0];
+
+    m_id = m_recvBuffer[1];
+
+    for (std::size_t i = 0; i < 16; ++i)
+        m_auth[i] = m_recvBuffer[i + 4];
+}
+
+uint8_t Request::type() const
+{
+    return m_type;
+}
+
+uint8_t Request::id() const
+{
+    return m_type;
+}
+
+const std::array<uint8_t, 16>& Request::auth() const
+{
+    return m_auth;
+}
