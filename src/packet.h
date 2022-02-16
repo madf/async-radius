bb43542f406dfdb0f7cf59133f36678296eca883
Elena Mamontova <helenh463@gmail.com>
2022-02-16 11:40:02 +0200

    Initial. Class name Request changed to Packet. Method make_sendBuffer() and variable m_sendBuffer added. File name request.h changed to packet.h.

--- src/packet.h
+++ src/packet.h
@@ -1,21 +1,24 @@
-#ifndef REQUEST_H
-#define REQUEST_H
+#ifndef PACKET_H
+#define PACKET_H
 
 #include <array>
+#include <vector>
 #include <cstdint> //uint8_t, uint32_t
 
-class Request
+class Packet
 {
     public:
-        Request(const std::array<uint8_t, 4096>& m_recvBuffer);
+        Packet(const std::array<uint8_t, 4096>& m_recvBuffer);
         uint8_t type() const;
         uint8_t id() const;
         const std::array<uint8_t, 16>& auth() const;
+        std::vector<uint8_t>& make_sendBuffer();
 
     private:
         uint8_t m_type;
         uint8_t m_id;
         std::array<uint8_t, 16> m_auth;
+        std::vector<uint8_t> m_sendBuffer;
 };
 
 #endif
