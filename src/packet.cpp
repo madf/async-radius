9e25208e9383acca7b07fa5500f5b5773b2e12e4
Elena Mamontova <helenh463@gmail.com>
2022-02-16 11:48:26 +0200

    Initial. File name request.cpp changed to packet.cpp. Class name Request changed to Packet. Header files "packet_codes.h", <string>, <iostream>, <openssl/md5.h> added. Definition of function make_sendBuffer() added.

--- src/packet.cpp
+++ src/packet.cpp
@@ -1,6 +1,10 @@
-#include "request.h"
+#include "packet.h"
+#include "packet_codes.h"
+#include <string>
+#include <iostream>
+#include <openssl/md5.h>
 
-Request::Request(const std::array<uint8_t, 4096>& m_recvBuffer)
+Packet::Packet(const std::array<uint8_t, 4096>& m_recvBuffer)
 {
     m_type = m_recvBuffer[0];
 
@@ -10,17 +14,55 @@ Request::Request(const std::array<uint8_t, 4096>& m_recvBuffer)
         m_auth[i] = m_recvBuffer[i + 4];
 }
 
-uint8_t Request::type() const
+uint8_t Packet::type() const
 {
     return m_type;
 }
 
-uint8_t Request::id() const
+uint8_t Packet::id() const
 {
     return m_id;
 }
 
-const std::array<uint8_t, 16>& Request::auth() const
+const std::array<uint8_t, 16>& Packet::auth() const
 {
     return m_auth;
+}
+
+std::vector<uint8_t>& Packet::make_sendBuffer()
+{
+    m_sendBuffer.resize(20);
+    if (m_type == ACCESS_REQUEST)
+    {
+         m_sendBuffer[0] = ACCESS_ACCEPT;
+         std::cout << "Packet type: ACCESS_ACCEPT\n";
+    }
+    else
+    {
+        m_sendBuffer[0] = ACCESS_REJECT;
+        std::cout << "Packet type: ACCESS_REJECT\n";
+    }
+
+    m_sendBuffer[1] = m_id;
+    std::cout << "Packet ID: " << static_cast<size_t>(m_id) << "\n";
+
+    m_sendBuffer[2] = 0;
+    m_sendBuffer[3] = 20;
+
+    for (size_t i = 0; i < 16; ++i)
+        m_sendBuffer[i + 4] = m_auth[i];
+
+    std::string secr("secret");
+
+    for (size_t i = 0; i < 6; ++i)
+        m_sendBuffer[i + 20] = secr[i];
+
+    std::array<uint8_t, 16> md;
+
+    MD5(m_sendBuffer.data(), 20 + secr.length(), md.data());
+
+    for (size_t i = 0; i < 16; ++i)
+        m_sendBuffer[i + 4] = md[i];
+
+    return m_sendBuffer;
 }
