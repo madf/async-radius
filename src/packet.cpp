0fa2f99e4da921b5a3533b45fcb1abe56dacdda2
Elena Mamontova <helenh463@gmail.com>
2022-02-21 13:27:03 +0200

    Initial. Return value of makeSendBuffer() method changed to value. Variable std::vector<uint8_t> sendBuffer added in the function makeSendBuffer(). Variable m_sendBuffer replaced by variable sendBuffer.

--- src/packet.cpp
+++ src/packet.cpp
@@ -29,41 +29,42 @@ const std::array<uint8_t, 16>& Packet::auth() const
     return m_auth;
 }
 
-std::vector<uint8_t>& Packet::makeSendBuffer()
+std::vector<uint8_t> Packet::makeSendBuffer()
 {
-    m_sendBuffer.resize(20);
+    std::vector<uint8_t> sendBuffer;
+    sendBuffer.resize(20);
 
     if (m_type == ACCESS_REQUEST)
     {
-         m_sendBuffer[0] = ACCESS_ACCEPT;
+         sendBuffer[0] = ACCESS_ACCEPT;
          std::cout << "Packet type: ACCESS_ACCEPT\n";
     }
     else
     {
-        m_sendBuffer[0] = ACCESS_REJECT;
+        sendBuffer[0] = ACCESS_REJECT;
         std::cout << "Packet type: ACCESS_REJECT\n";
     }
 
-    m_sendBuffer[1] = m_id;
+    sendBuffer[1] = m_id;
     std::cout << "Packet ID: " << static_cast<size_t>(m_id) << "\n";
 
-    m_sendBuffer[2] = 0;
-    m_sendBuffer[3] = 20;
+    sendBuffer[2] = 0;
+    sendBuffer[3] = 20;
 
     for (size_t i = 0; i < 16; ++i)
-        m_sendBuffer[i + 4] = m_auth[i];
+        sendBuffer[i + 4] = m_auth[i];
 
     std::string secr("secret");
 
     for (size_t i = 0; i < 6; ++i)
-        m_sendBuffer[i + 20] = secr[i];
+        sendBuffer[i + 20] = secr[i];
 
     std::array<uint8_t, 16> md;
 
-    MD5(m_sendBuffer.data(), 20 + secr.length(), md.data());
+    MD5(sendBuffer.data(), 20 + secr.length(), md.data());
 
     for (size_t i = 0; i < 16; ++i)
-        m_sendBuffer[i + 4] = md[i];
+        sendBuffer[i + 4] = md[i];
 
-    return m_sendBuffer;
+    return sendBuffer;
 }
