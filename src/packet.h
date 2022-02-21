0986c17673d363c9e4bdaa3db65066141434be0a
Elena Mamontova <helenh463@gmail.com>
2022-02-21 13:23:22 +0200

    Initial. Return value of makeSendBuffer() method changed to value. Variable m_sendBuffer removed.

--- src/packet.h
+++ src/packet.h
@@ -12,13 +12,12 @@ class Packet
         uint8_t type() const;
         uint8_t id() const;
         const std::array<uint8_t, 16>& auth() const;
-        std::vector<uint8_t>& makeSendBuffer();
+        std::vector<uint8_t> makeSendBuffer();
 
     private:
         uint8_t m_type;
         uint8_t m_id;
         std::array<uint8_t, 16> m_auth;
-        std::vector<uint8_t> m_sendBuffer;
 };
 
 #endif
