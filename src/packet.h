1bf19b585ca81cbe55de5128c903cd8e9c9e6540
Elena Mamontova <helenh463@gmail.com>
2022-04-26 15:57:14 +0300

    Initial. Header file "attribute.h" added. Method attributes() added. Vector m_attributes added.

--- src/packet.h
+++ src/packet.h
@@ -1,6 +1,7 @@
 #ifndef PACKET_H
 #define PACKET_H
 
+#include "attribute.h"
 #include <array>
 #include <vector>
 #include <string>
@@ -14,12 +15,14 @@ class Packet
         uint8_t type() const;
         uint8_t id() const;
         const std::array<uint8_t, 16>& auth() const;
+        const std::vector<Attribute*>& attributes() const;
         const std::vector<uint8_t> makeSendBuffer(const std::string& secret);
 
     private:
         uint8_t m_type;
         uint8_t m_id;
         std::array<uint8_t, 16> m_auth;
+        std::vector<Attribute*> m_attributes;
 };
 
 #endif
