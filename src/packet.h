ff375c1812f5d3b38c2d746a89a75f3db820d30a
Elena Mamontova <helenh463@gmail.com>
2022-07-01 15:23:20 +0300

    Initial. Parameters data and size replaced by attributeValue and attributeValueSize in the makeAttribute() function.

--- src/packet.h
+++ src/packet.h
@@ -18,7 +18,7 @@ class Packet
         const std::array<uint8_t, 16>& auth() const;
         const std::vector<Attribute*>& attributes() const;
         const std::vector<uint8_t> makeSendBuffer(const std::string& secret);
-        Attribute* makeAttribute(uint8_t type, const uint8_t* data, size_t size);
+        Attribute* makeAttribute(uint8_t type, const uint8_t* attributeValue, size_t attributeValueSize, std::string secret, std::array<uint8_t, 16> auth);
 
     private:
         uint8_t m_type;
