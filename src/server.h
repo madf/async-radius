194bb69f72e3644c49f92c71d8aeb055beaa2bb4
Elena Mamontova <helenh463@gmail.com>
2022-10-31 16:41:26 +0200

    Initial. Parameter const std::string& secret removed in startReceive and handleReceive functions. Variable m_secret added in the class Server.

--- src/server.h
+++ src/server.h
@@ -12,14 +12,15 @@ class Server
         Server(boost::asio::io_service& io_service, const std::string& secret);
 
     private:
-        void startReceive(const std::string& secret);
-        void handleReceive(const std::string& secret, const boost::system::error_code& error, std::size_t bytes);
+        void startReceive();
+        void handleReceive(const boost::system::error_code& error, std::size_t bytes);
         void handleSend(const boost::system::error_code& error, std::size_t bytes_transferred);
         Packet makeResponse(const Packet& request);
 
         boost::asio::ip::udp::socket m_socket;
         boost::asio::ip::udp::endpoint m_remoteEndpoint;
         std::array<uint8_t, 4096> m_recvBuffer;
+        std::string m_secret;
 };
 
 #endif
