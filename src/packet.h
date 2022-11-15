c3dc40fc0aea91e8db968f9fadcdc46516d5918f
Elena Mamontova <helenh463@gmail.com>
2022-11-15 16:03:51 +0200

    Initial. Header file "vendor_attribute.h" added. The vendorAttributes and makeVendorAttribute methods declaration added to class Packet. The m_vendorAttributes variable class member added to class Packet.

--- src/packet.h
+++ src/packet.h
@@ -2,6 +2,7 @@
 #define PACKET_H
 
 #include "attribute.h"
+#include "vendor_attribute.h"
 #include <array>
 #include <vector>
 #include <string>
@@ -17,14 +18,17 @@ class Packet
         uint8_t id() const;
         const std::array<uint8_t, 16>& auth() const;
         const std::vector<Attribute*>& attributes() const;
+        const std::vector<VendorAttribute*>& vendorAttributes() const;
         const std::vector<uint8_t> makeSendBuffer(const std::string& secret);
         Attribute* makeAttribute(uint8_t type, const uint8_t* data, size_t size, const std::string& secret, const std::array<uint8_t, 16>& auth);
+        VendorAttribute* makeVendorAttribute(uint8_t type, const uint8_t* data, size_t size);
 
     private:
         uint8_t m_type;
         uint8_t m_id;
         std::array<uint8_t, 16> m_auth;
         std::vector<Attribute*> m_attributes;
+        std::vector<VendorAttribute*> m_vendorAttributes;
 };
 
 #endif
