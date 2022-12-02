a3cd7a4340c9bb539c3050dca34a21a273be4bb6
Elena Mamontova <helenh463@gmail.com>
2022-12-02 13:17:11 +0200

    Initial. Class VendorAttribute removed. The methods name(), type() added, the keyword override removed in the class VendorSpecific.

--- src/vendor_attribute.h
+++ src/vendor_attribute.h
@@ -5,32 +5,19 @@
 #include <vector>
 #include <cstdint> //uint8_t, uint32_t
 
-class VendorAttribute
-{
-    public:
-        VendorAttribute(uint8_t type);
-        virtual ~VendorAttribute() = default;
-        std::string name() const;
-        uint8_t type() const;
-        virtual std::string value() const = 0;
-        virtual uint8_t vendorType() const = 0;
-        virtual std::string vendorId() const = 0 ;
-        virtual std::vector<uint8_t> toVector() const = 0;
-
-    private:
-        uint8_t m_type;
-};
-
-class VendorSpecific: public VendorAttribute
+class VendorSpecific
 {
     public:
         VendorSpecific(uint8_t type, const uint8_t* data, size_t size);
         VendorSpecific(uint8_t type, uint32_t vendorId, uint8_t vendorType, const std::vector<uint8_t>& vendorValue);
-        std::string value() const override;
-        uint8_t vendorType() const override;
-        std::string vendorId() const override;
-        std::vector<uint8_t> toVector() const override;
+        std::string name() const;
+        uint8_t type() const;
+        std::string value() const;
+        uint8_t vendorType() const;
+        std::string vendorId() const;
+        std::vector<uint8_t> toVector() const;
     private:
+        uint8_t m_type;
         uint32_t m_vendorId;
         uint8_t m_vendorType;
         std::vector<uint8_t> m_value;
