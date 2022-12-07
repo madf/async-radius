eb3b3cb66f2a3f951bf433e7e049f6a213d26333
Elena Mamontova <helenh463@gmail.com>
2022-12-07 16:20:41 +0200

    Initial. Parameter 'type' removed in the VendorSpecific constructors declaration. Function type() and variable m_type removed.

--- src/vendor_attribute.h
+++ src/vendor_attribute.h
@@ -8,16 +8,14 @@
 class VendorSpecific
 {
     public:
-        VendorSpecific(uint8_t type, const uint8_t* data);
-        VendorSpecific(uint8_t type, uint32_t vendorId, uint8_t vendorType, const std::vector<uint8_t>& vendorValue);
+        VendorSpecific(const uint8_t* data);
+        VendorSpecific(uint32_t vendorId, uint8_t vendorType, const std::vector<uint8_t>& vendorValue);
         std::string name() const;
-        uint8_t type() const;
         std::string value() const;
         uint8_t vendorType() const;
         uint32_t vendorId() const;
         std::vector<uint8_t> toVector() const;
     private:
-        uint8_t m_type;
         uint32_t m_vendorId;
         uint8_t m_vendorType;
         std::vector<uint8_t> m_value;
