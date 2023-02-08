0c6ea51c12f2ae7ae583a3f6d04e73bef85b64f7
Elena Mamontova <helenh463@gmail.com>
2023-02-08 16:27:14 +0200

    New file dictionaries.cpp added.

---
+++ src/dictionaries.cpp
@@ -0,0 +1,26 @@
+#include "dictionaries.h"
+#include <utility>
+#include <iostream>
+
+BasicDictionary::BasicDictionary(uint8_t type, std::string name)
+    : m_type(type),
+      m_name(name)
+{
+}
+
+uint8_t BasicDictionary::getType() const
+{
+    return m_type;
+}
+
+std::string BasicDictionary::getName() const
+{
+    return m_name;
+}
+
+void BasicDictionary::add()
+{
+    m_rightDict.insert(std::pair<uint8_t, std::string>(m_type, m_name));
+    m_reverseDict.insert(std::pair<std::string, uint8_t>(m_name, m_type));
+    return;
+}
