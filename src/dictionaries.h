6d555ae9fd3fa03769a7d01df62a8fb2968981ba
Elena Mamontova <helenh463@gmail.com>
2023-02-09 14:48:11 +0200

    Class DependentDictionary declaration added.

--- src/dictionaries.h
+++ src/dictionaries.h
@@ -18,4 +18,21 @@ class BasicDictionary
         std::map<uint8_t, std::string> m_rightDict;
         std::map<std::string, uint8_t> m_reverseDict;
 };
+
+class DependentDictionary
+{
+    public:
+        DependentDictionary(uint8_t type, const std::string name, const std::string attributeName);
+        uint8_t getType() const;
+        std::string getName() const;
+        std::string getAttributeName() const;
+        void add();
+    private:
+        uint8_t m_type;
+        std::string m_name;
+        std::string m_attributeName;
+        std::map<std::pair<std::string, uint8_t>, std::string> m_rightDict;
+        std::map<std::pair<std::string, std::string>, uint8_t> m_reverseDict;
+};
+
 #endif
