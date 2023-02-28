7e79887568d48bc84d22a169059dc03a9ed4b56d
Elena Mamontova <helenh463@gmail.com>
2023-02-28 14:41:31 +0200

    Empty constructors removed in the classes BasicDictionary, DependentDictionary, Dictionaries.

--- src/dictionaries.cpp
+++ src/dictionaries.cpp
@@ -1,10 +1,6 @@
 #include "dictionaries.h"
 #include <utility>
 
-BasicDictionary::BasicDictionary()
-{
-}
-
 std::string BasicDictionary::getName(uint8_t type) const
 {
     return m_rightDict.at(type);
@@ -21,10 +17,6 @@ void BasicDictionary::add(uint8_t type, const std::string& name)
     m_reverseDict.emplace(name, type);
 }
 
-DependentDictionary::DependentDictionary()
-{
-}
-
 std::string DependentDictionary::getName(const std::string& attributeName, uint8_t type) const
 {
     return m_rightDict.at(std::make_pair(attributeName, type));
@@ -39,10 +31,6 @@ void DependentDictionary::add(uint8_t type, const std::string& name, const std::
 {
     m_rightDict.emplace(std::make_pair(attributeName, type), name);
     m_reverseDict.emplace(std::make_pair(attributeName, name), type);
-}
-
-Dictionaries::Dictionaries()
-{
 }
 
 const BasicDictionary& Dictionaries::getAttributes() const
