c699681abe74ee099f4581110d58d8db90796f4d
Elena Mamontova <helenh463@gmail.com>
2023-03-07 13:44:10 +0200

    Parameter filePath added to Dictonaries constructor declaration.

--- src/dictionaries.h
+++ src/dictionaries.h
@@ -32,7 +32,7 @@ class DependentDictionary
 class Dictionaries
 {
     public:
-        Dictionaries() = default;
+        Dictionaries(const std::string& filePath);
         const BasicDictionary& attributes() const { return m_attributes; }
         const BasicDictionary& vendorNames() const { return m_vendorNames; }
         const DependentDictionary& attributeValues() const { return m_attributeValues; }
