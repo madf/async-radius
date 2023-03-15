f708288e22cd5a66d0ffa713dd0f070e6fe7080b
Elena Mamontova <helenh463@gmail.com>
2023-03-15 16:53:37 +0200

    Getting of attribute name, attribute type and parameter keyword added to the constructor Dictionaries.

--- src/dictionaries.cpp
+++ src/dictionaries.cpp
@@ -35,7 +35,7 @@ void DependentDictionary::add(uint8_t type, const std::string& name, const std::
     m_reverseDict.emplace(std::make_pair(attributeName, name), type);
 }
 
-Dictionaries::Dictionaries(const std::string& filePath)
+Dictionaries::Dictionaries(const std::string& filePath, const std::string& keyword)
 {
     std::ifstream stream(filePath);
     if (!stream)
@@ -43,5 +43,22 @@ Dictionaries::Dictionaries(const std::string& filePath)
 
     std::string line;
     while (std::getline(stream, line))
-        std::cout << line << "\n";
+    {
+        size_t firstPosKeyword = line.find(keyword);
+        if (firstPosKeyword != line.npos)
+        {
+            size_t lastPosKeyword = line.find_first_of(" \t", firstPosKeyword) - 1;
+
+            size_t firstPosName = line.find_first_not_of(" \t", lastPosKeyword + 1);
+            size_t lastPosName = line.find_first_of("  \t", firstPosName) - 1;
+            std::string name = line.substr(firstPosName, lastPosName - firstPosName + 1);
+
+            size_t firstPosType = line.find_first_not_of(" \t", lastPosName + 1);
+            size_t lastPosType= line.find_first_of("  \t", firstPosType) - 1;
+            std::string type = line.substr(firstPosType, lastPosType - firstPosType + 1);
+
+            if (name == "User-Name" || name == "User-Password")
+                std::cout << name << ": " << type << "\n";
+        }
+    }
 }
