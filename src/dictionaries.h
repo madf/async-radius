55e0726626ee431fc1f2376b530d88832f6b058a
Elena Mamontova <helenh463@gmail.com>
2023-03-20 16:56:51 +0200

    The variable name and function name 'type' changed to 'code' in the classes BasicDictionary, DependentDictionary.

--- src/dictionaries.h
+++ src/dictionaries.h
@@ -9,9 +9,9 @@ class BasicDictionary
 {
     public:
         BasicDictionary() = default;
-        std::string name(uint8_t type) const;
-        uint8_t type(const std::string& name) const;
-        void add(uint8_t type, const std::string& name);
+        std::string name(uint8_t code) const;
+        uint8_t code(const std::string& name) const;
+        void add(uint8_t code, const std::string& name);
     private:
         std::map<uint8_t, std::string> m_rightDict;
         std::map<std::string, uint8_t> m_reverseDict;
@@ -21,9 +21,9 @@ class DependentDictionary
 {
     public:
         DependentDictionary() = default;
-        std::string name(const std::string& attributeName, uint8_t type) const;
-        uint8_t type(const std::string& attributeName, const std::string& name) const;
-        void add(uint8_t type, const std::string& name, const std::string& attributeName);
+        std::string name(const std::string& attributeName, uint8_t code) const;
+        uint8_t code(const std::string& attributeName, const std::string& name) const;
+        void add(uint8_t code, const std::string& name, const std::string& attributeName);
     private:
         std::map<std::pair<std::string, uint8_t>, std::string> m_rightDict;
         std::map<std::pair<std::string, std::string>, uint8_t> m_reverseDict;
