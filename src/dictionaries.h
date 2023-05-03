d215ba2d74da4f535dc738d5b2b9fb41e9e0b582
Elena Mamontova <helenh463@gmail.com>
2023-05-03 16:29:51 +0300

    Reference added to pparameter in the rightDict, reverseDict methods of BasicDictionary and DependentDictionary classes. Method append declaration added in the BasicDictionary class.

--- src/dictionaries.h
+++ src/dictionaries.h
@@ -12,8 +12,9 @@ class BasicDictionary
         std::string name(uint32_t code) const;
         uint32_t code(const std::string& name) const;
         void add(uint32_t code, const std::string& name);
-        const std::map<uint32_t, std::string> rightDict() const { return m_rightDict; }
-        const std::map<std::string, uint32_t> reverseDict() const { return m_reverseDict; }
+        void append(const BasicDictionary& basicDict);
+        const std::map<uint32_t, std::string>& rightDict() const { return m_rightDict; }
+        const std::map<std::string, uint32_t>& reverseDict() const { return m_reverseDict; }
     private:
         std::map<uint32_t, std::string> m_rightDict;
         std::map<std::string, uint32_t> m_reverseDict;
@@ -26,8 +27,8 @@ class DependentDictionary
         std::string name(const std::string& dependencyName, uint32_t code) const;
         uint32_t code(const std::string& dependencyName, const std::string& name) const;
         void add(uint32_t code, const std::string& name, const std::string& dependencyName);
-        const std::map<std::pair<std::string, uint32_t>, std::string> rightDict() const { return m_rightDict; }
-        const std::map<std::pair<std::string, std::string>, uint32_t> reverseDict() const { return m_reverseDict; }
+        const std::map<std::pair<std::string, uint32_t>, std::string>& rightDict() const { return m_rightDict; }
+        const std::map<std::pair<std::string, std::string>, uint32_t>& reverseDict() const { return m_reverseDict; }
     private:
         std::map<std::pair<std::string, uint32_t>, std::string> m_rightDict;
         std::map<std::pair<std::string, std::string>, uint32_t> m_reverseDict;
