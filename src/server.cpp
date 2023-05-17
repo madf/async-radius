1c4487db144531cd90f526497327946a057b9e2d
Elena Mamontova <helenh463@gmail.com>
2023-05-17 16:01:22 +0300

    Parameter m_dictionaries added to Packet constructor when makeResponse method call. Mathod dictionaries call replaced by m_dictiionaries object.

--- src/server.cpp
+++ src/server.cpp
@@ -32,13 +32,13 @@ void Server::printPacket(const Packet& p)
 
     std::cout << "Attributes:\n";
     for (const auto& ap : p.attributes())
-        std::cout << "\t" << dictionaries().attributes().name(ap->type()) << ": " << ap->toString() << "\n";
+        std::cout << "\t" << m_dictionaries.attributes().name(ap->type()) << ": " << ap->toString() << "\n";
 
     for (const auto& ap : p.vendorSpecific())
     {
-        const std::string vendorName = dictionaries().vendorNames().name(ap->vendorId());
+        const std::string vendorName = m_dictionaries.vendorNames().name(ap->vendorId());
         std::cout << "\t" << ap->name() << ": " << vendorName << "\n";
-        std::cout << "\t" << dictionaries().vendorAttributes().name(vendorName, ap->vendorType()) << ": " << ap->toString() << "\n";
+        std::cout << "\t" << m_dictionaries.vendorAttributes().name(vendorName, ap->vendorType()) << ": " << ap->toString() << "\n";
     }
 }
 
@@ -90,7 +90,7 @@ void Server::handleReceive(const error_code& error, std::size_t bytes)
 
     try
     {
-        Packet packet = makeResponse(Packet(m_recvBuffer, bytes, m_secret));
+        Packet packet = makeResponse(Packet(m_recvBuffer, bytes, m_secret, m_dictionaries));
 
         std::cout << "Response packet\n";
         printPacket(packet);
@@ -115,19 +115,19 @@ Packet Server::makeResponse(const Packet& request)
     printPacket(request);
 
     std::vector<Attribute*> attributes;
-    attributes.push_back(new String(dictionaries().attributes().code("User-Name"), "test"));
-    attributes.push_back(new Integer(dictionaries().attributes().code("NAS-Port"), 20));
+    attributes.push_back(new String(m_dictionaries.attributes().code("User-Name"), "test"));
+    attributes.push_back(new Integer(m_dictionaries.attributes().code("NAS-Port"), 20));
     std::array<uint8_t, 4> address {127, 104, 22, 17};
-    attributes.push_back(new IpAddress(dictionaries().attributes().code("NAS-IP-Address"), address));
-    attributes.push_back(new Encrypted(dictionaries().attributes().code("User-Password"), "password123"));
+    attributes.push_back(new IpAddress(m_dictionaries.attributes().code("NAS-IP-Address"), address));
+    attributes.push_back(new Encrypted(m_dictionaries.attributes().code("User-Password"), "password123"));
     std::vector<uint8_t> bytes {'1', '2', '3', 'a', 'b', 'c'};
-    attributes.push_back(new Bytes(dictionaries().attributes().code("Callback-Number"), bytes));
+    attributes.push_back(new Bytes(m_dictionaries.attributes().code("Callback-Number"), bytes));
     std::vector<uint8_t> chapPassword {'1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g' };
-    attributes.push_back(new ChapPassword(dictionaries().attributes().code("CHAP-Password"), 1, chapPassword));
+    attributes.push_back(new ChapPassword(m_dictionaries.attributes().code("CHAP-Password"), 1, chapPassword));
 
     std::vector<VendorSpecific*> vendorSpecific;
     std::vector<uint8_t> vendorValue {'0', '0', '0', '3'};
-    vendorSpecific.push_back(new VendorSpecific(dictionaries().vendorNames().code("Dlink"), dictionaries().vendorAttributes().code("Dlink", "Dlink-User-Level"), vendorValue));
+    vendorSpecific.push_back(new VendorSpecific(m_dictionaries.vendorNames().code("Dlink"), m_dictionaries.vendorAttributes().code("Dlink", "Dlink-User-Level"), vendorValue));
 
     if (request.type() == ACCESS_REQUEST)
         return Packet(ACCESS_ACCEPT, request.id(), request.auth(), attributes, vendorSpecific);
