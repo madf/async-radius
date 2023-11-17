d13a857dcc5d6ed7853a6c910af0d22d483ee4a2
Elena Mamontova <helenh463@gmail.com>
2023-11-17 17:15:24 +0200

    The keyword const added to parameter response in asyncSend method.

--- src/server.cpp
+++ src/server.cpp
@@ -34,7 +34,7 @@ void Server::asyncReceive(const std::function<void(const error_code&, const Pack
        [this, callback](const error_code& error, std::size_t bytes) {handleReceive(error, bytes, callback);});
 }
 
-void Server::asyncSend(Packet& response, const std::function<void(const error_code&)>& callback)
+void Server::asyncSend(const Packet& response, const std::function<void(const error_code&)>& callback)
 {
     m_socket.async_send_to(boost::asio::buffer(response.makeSendBuffer(m_secret)), m_remoteEndpoint,
        [this, callback](const error_code& ec, std::size_t /*bytesTransferred*/) {handleSend(ec, callback);});
