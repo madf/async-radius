125674fc7c9f01fbd7c6a7c28027e14c68c6eda0
Elena Mamontova <helenh463@gmail.com>
2023-12-14 16:05:59 +0200

    The namespace RadProto added.

--- src/server.cpp
+++ src/server.cpp
@@ -2,73 +2,76 @@
 #include "error.h"
 #include "packet_codes.h"
 
-using boost::asio::ip::udp;
-using boost::system::error_code;
-
-namespace pls = std::placeholders;
-
-std::string packetTypeToString(int type)
+namespace RadProto
 {
-    switch (type)
+    using boost::asio::ip::udp;
+    using boost::system::error_code;
+
+    namespace pls = std::placeholders;
+
+    std::string packetTypeToString(int type)
     {
-        case ACCESS_REQUEST: return "ACCESS_REQUEST";
-        case ACCESS_ACCEPT: return "ACCESS_ACCEPT";
-        case ACCESS_REJECT: return "ACCESS_REJECT";
-        case ACCOUNTING_REQUEST: return "ACCOUNTING_REQUEST";
-        case ACCOUNTING_RESPONSE: return "ACCOUNTING_RESPONSE";
-        case ACCESS_CHALLENGE: return "ACCESS_CHALLENGE";
-        case STATUS_SERVER: return "STATUS_SERVER";
-        case STATUS_CLIENT: return "STATUS_CLIENT";
+        switch (type)
+        {
+            case ACCESS_REQUEST: return "ACCESS_REQUEST";
+            case ACCESS_ACCEPT: return "ACCESS_ACCEPT";
+            case ACCESS_REJECT: return "ACCESS_REJECT";
+            case ACCOUNTING_REQUEST: return "ACCOUNTING_REQUEST";
+            case ACCOUNTING_RESPONSE: return "ACCOUNTING_RESPONSE";
+            case ACCESS_CHALLENGE: return "ACCESS_CHALLENGE";
+            case STATUS_SERVER: return "STATUS_SERVER";
+            case STATUS_CLIENT: return "STATUS_CLIENT";
+        }
+        return "uncnown";
     }
-    return "uncnown";
-}
 
-Server::Server(boost::asio::io_service& io_service, const std::string& secret)
-    : m_socket(io_service, udp::endpoint(udp::v4(), 9999)),
-      m_secret(secret)
-{
-}
-
-void Server::asyncReceive(const std::function<void(const error_code&, const std::optional<Packet>&)>& callback)
-{
-    m_socket.async_receive_from(boost::asio::buffer(m_recvBuffer), m_remoteEndpoint,
-       [this, callback](const error_code& error, std::size_t bytes) {handleReceive(error, bytes, callback);});
-}
-
-void Server::asyncSend(const Packet& response, const std::function<void(const error_code&)>& callback)
-{
-    m_socket.async_send_to(boost::asio::buffer(response.makeSendBuffer(m_secret)), m_remoteEndpoint,
-       [this, callback](const error_code& ec, std::size_t /*bytesTransferred*/) {handleSend(ec, callback);});
-}
-
-void Server::handleReceive(const error_code& error, std::size_t bytes, std::function<void(const error_code&, const std::optional<Packet>&)> callback)
-{
-        std::optional<Packet> oEmpty;
-        std::optional<Packet> oRequest;
-
-        if (bytes < 20)
-            callback(lib::Error::numberBytesLess20, oEmpty);
-
-    try
+    Server::Server(boost::asio::io_service& io_service, const std::string& secret)
+        : m_socket(io_service, udp::endpoint(udp::v4(), 9999)),
+          m_secret(secret)
     {
-        const Packet request = Packet(m_recvBuffer, bytes, m_secret);
-        oRequest = request;
     }
-    catch (const std::runtime_error& exception)
+
+    void Server::asyncReceive(const std::function<void(const error_code&, const std::optional<Packet>&)>& callback)
     {
-        if (std::string (exception.what()) == "lib::Error::numberBytesLess20")
-            callback(lib::Error::numberBytesLess20, oEmpty);
-
-        if (std::string (exception.what()) == "lib::Error::requestLengthShort")
-            callback(lib::Error::requestLengthShort, oEmpty);
-
-        if (std::string (exception.what()) == "lib::Error::eapMessageAttributeError")
-            callback(lib::Error::eapMessageAttributeError, oEmpty);
+        m_socket.async_receive_from(boost::asio::buffer(m_recvBuffer), m_remoteEndpoint,
+           [this, callback](const error_code& error, std::size_t bytes) {handleReceive(error, bytes, callback);});
     }
-    callback(error, oRequest);
-}
 
-void Server::handleSend(const error_code& ec, std::function<void(const error_code&)> callback)
-{
-    callback(ec);
+    void Server::asyncSend(const Packet& response, const std::function<void(const error_code&)>& callback)
+    {
+        m_socket.async_send_to(boost::asio::buffer(response.makeSendBuffer(m_secret)), m_remoteEndpoint,
+           [this, callback](const error_code& ec, std::size_t /*bytesTransferred*/) {handleSend(ec, callback);});
+    }
+
+    void Server::handleReceive(const error_code& error, std::size_t bytes, std::function<void(const error_code&, const std::optional<Packet>&)> callback)
+    {
+            std::optional<Packet> oEmpty;
+            std::optional<Packet> oRequest;
+
+            if (bytes < 20)
+                callback(Error::numberBytesLess20, oEmpty);
+
+        try
+        {
+            const Packet request = Packet(m_recvBuffer, bytes, m_secret);
+            oRequest = request;
+        }
+        catch (const std::runtime_error& exception)
+        {
+            if (std::string (exception.what()) == "Error::numberBytesLess20")
+                callback(Error::numberBytesLess20, oEmpty);
+
+            if (std::string (exception.what()) == "Error::requestLengthShort")
+                callback(Error::requestLengthShort, oEmpty);
+
+            if (std::string (exception.what()) == "Error::eapMessageAttributeError")
+                callback(Error::eapMessageAttributeError, oEmpty);
+        }
+        callback(error, oRequest);
+    }
+
+    void Server::handleSend(const error_code& ec, std::function<void(const error_code&)> callback)
+    {
+        callback(ec);
+    }
 }
