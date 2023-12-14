fafcb8f5f0fba8b57610cde54bce42d7ebd2caef
Elena Mamontova <helenh463@gmail.com>
2023-12-14 16:29:06 +0200

    The namespace RadProto added.

--- include/radproto/packet.h
+++ include/radproto/packet.h
@@ -8,26 +8,29 @@
 #include <string>
 #include <cstdint> //uint8_t, uint32_t
 
-class Packet
+namespace RadProto
 {
-    public:
-        Packet(const std::array<uint8_t, 4096>& m_recvBuffer, size_t bytes, const std::string& secret);
-        Packet(uint8_t type, uint8_t id, const std::array<uint8_t, 16>& auth, const std::vector<Attribute*>& attributes, const std::vector<VendorSpecific*>& vendorSpecific);
-        ~Packet();
-        uint8_t type() const { return m_type; }
-        uint8_t id() const { return m_id; };
-        const std::array<uint8_t, 16>& auth() const { return m_auth; }
-        const std::vector<Attribute*>& attributes() const { return m_attributes; }
-        const std::vector<VendorSpecific*>& vendorSpecific() const { return m_vendorSpecific; }
-        const std::vector<uint8_t> makeSendBuffer(const std::string& secret) const;
-        Attribute* makeAttribute(uint8_t type, const uint8_t* data, size_t size, const std::string& secret, const std::array<uint8_t, 16>& auth);
+    class Packet
+    {
+        public:
+            Packet(const std::array<uint8_t, 4096>& m_recvBuffer, size_t bytes, const std::string& secret);
+            Packet(uint8_t type, uint8_t id, const std::array<uint8_t, 16>& auth, const std::vector<Attribute*>& attributes, const std::vector<VendorSpecific*>& vendorSpecific);
+            ~Packet();
+            uint8_t type() const { return m_type; }
+            uint8_t id() const { return m_id; };
+            const std::array<uint8_t, 16>& auth() const { return m_auth; }
+            const std::vector<Attribute*>& attributes() const { return m_attributes; }
+            const std::vector<VendorSpecific*>& vendorSpecific() const { return m_vendorSpecific; }
+            const std::vector<uint8_t> makeSendBuffer(const std::string& secret) const;
+            Attribute* makeAttribute(uint8_t type, const uint8_t* data, size_t size, const std::string& secret, const std::array<uint8_t, 16>& auth);
 
-    private:
-        uint8_t m_type;
-        uint8_t m_id;
-        std::array<uint8_t, 16> m_auth;
-        std::vector<Attribute*> m_attributes;
-        std::vector<VendorSpecific*> m_vendorSpecific;
-};
+        private:
+            uint8_t m_type;
+            uint8_t m_id;
+            std::array<uint8_t, 16> m_auth;
+            std::vector<Attribute*> m_attributes;
+            std::vector<VendorSpecific*> m_vendorSpecific;
+    };
+}
 
 #endif
