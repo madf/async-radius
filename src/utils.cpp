98f4d90036e9cf0399bc6a7e031de840a48c4fee
Elena Mamontova <helenh463@gmail.com>
2023-12-15 16:59:53 +0200

    The namespace RadProto added.

--- src/utils.cpp
+++ src/utils.cpp
@@ -2,8 +2,11 @@
 #include "attribute_types.h"
 #include <cstdint> //uint8_t, uint32_t
 
-std::string byteToHex(uint8_t byte)
+namespace RadProto
 {
-    static const std::string digits = "0123456789ABCDEF";
-    return {digits[byte / 16], digits[byte % 16]};
+    std::string byteToHex(uint8_t byte)
+    {
+        static const std::string digits = "0123456789ABCDEF";
+        return {digits[byte / 16], digits[byte % 16]};
+    }
 }
