896b1b96bcc6ecba35ca7261e2de88568430e2f7
Elena Mamontova <helenh463@gmail.com>
2023-12-29 14:14:10 +0200

    An alias with 'using' is declared instead of using the RadProto namespace. RadProto:: added to the make_error_code funktion.

--- src/error.cpp
+++ src/error.cpp
@@ -1,43 +1,43 @@
 #include "error.h"
 #include <string>
 
-namespace RadProto
+using Exception = RadProto::Exception;
+Exception::Exception(const boost::system::error_code& errorCode)
+    : std::runtime_error(errorCode.message())
 {
-    Exception::Exception(const boost::system::error_code& errorCode)
-        : std::runtime_error(errorCode.message())
-    {
-    }
+}
 
-    const char* ErrorCategory::name() const noexcept
-    {
-        return "library";
-    }
+using ErrorCategory = RadProto::ErrorCategory;
+const char* ErrorCategory::name() const noexcept
+{
+    return "library";
+}
 
-    std::string ErrorCategory::message(int ev) const noexcept
+using Error = RadProto::Error;
+std::string ErrorCategory::message(int ev) const noexcept
+{
+    switch (static_cast<Error>(ev))
     {
-        switch (static_cast<Error>(ev))
-        {
-            case Error::success:
-                return "No error";
-            case Error::numberOfBytesIsLessThan20:
-                return "Request length is less than 20 bytes";
-            case Error::requestLengthIsShort:
-                return "Request length is less than specified in the request";
-            case Error::eapMessageAttributeError:
-                return "The EAP-Message attribute is present, but the Message-Authenticator attribute is missing";
-            default:
-                return "(Unrecognized error)";
-        }
+        case Error::success:
+            return "No error";
+        case Error::numberOfBytesIsLessThan20:
+            return "Request length is less than 20 bytes";
+        case Error::requestLengthIsShort:
+            return "Request length is less than specified in the request";
+        case Error::eapMessageAttributeError:
+            return "The EAP-Message attribute is present, but the Message-Authenticator attribute is missing";
+        default:
+            return "(Unrecognized error)";
     }
+}
 
-    const boost::system::error_category& theCategory()
-    {
-        static const ErrorCategory instance;
-        return instance;
-    }
+const boost::system::error_category& theCategory()
+{
+    static const ErrorCategory instance;
+    return instance;
+}
 
-    boost::system::error_code make_error_code(Error e)
-    {
-        return boost::system::error_code(static_cast<int>(e), theCategory());
-    }
+boost::system::error_code RadProto::make_error_code(Error e)
+{
+    return boost::system::error_code(static_cast<int>(e), theCategory());
 }
