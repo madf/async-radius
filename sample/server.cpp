1bc419a85acab9a2a6910b10e84ab4c9f3f31dbd
Elena Mamontova <helenh463@gmail.com>
2024-02-13 15:48:13 +0200

    The parameter uint16_t port added to constructor Server definition and to m_radius initialization.

--- sample/server.cpp
+++ sample/server.cpp
@@ -5,8 +5,8 @@
 
 using boost::system::error_code;
 
-Server::Server(boost::asio::io_service& io_service, const std::string& secret, const std::string& filePath)
-    : m_radius(io_service, secret),
+Server::Server(boost::asio::io_service& io_service, const std::string& secret, uint16_t port, const std::string& filePath)
+    : m_radius(io_service, secret, port),
       m_dictionaries(filePath)
 {
     startReceive();
