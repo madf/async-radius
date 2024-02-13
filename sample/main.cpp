68c6b79ff0b2761c82167faa476daca5f9e1a2da
Elena Mamontova <helenh463@gmail.com>
2024-02-13 15:58:09 +0200

    Header files <stdlib.h>, <cstdint> is added. Parameter port is added to printHelp function. Variable uint16_t port is added, construction if() is chanded to if else(), parameter port checking is added to main function. Parameter port is added to constructor Server call.

--- sample/main.cpp
+++ sample/main.cpp
@@ -2,7 +2,9 @@
 #include "server.h"
 #include "error.h"
 #include <boost/asio.hpp>
+#include <stdlib.h>
 #include <iostream>
+#include <cstdint> //uint8_t, uint32_t
 
 using boost::system::error_code;
 
@@ -11,8 +13,9 @@ namespace
 
     void printHelp(const std::string& programName)
     {
-        std::cout << "Usage: " << programName << " -s/--secret <secret> [-h/--help] [-v/--version]\n"
+        std::cout << "Usage: " << programName << " -s/--secret <secret> [-p/--port <port>] [-h/--help] [-v/--version]\n"
                   << "\t --secret, -s <secret> - shared secret for password encryption by client and server;\n"
+                  << "\t --port, -p <port>     - port number for the socket;\n"
                   << "\t --help, -h            - print this help;\n"
                   << "\t --version, -v         - print version.\n";
     }
@@ -28,6 +31,7 @@ namespace
 int main(int argc, char* argv[])
 {
     std::string secret;
+    uint16_t port = 1812;
 
     if (argc < 2)
     {
@@ -43,12 +47,21 @@ int main(int argc, char* argv[])
             printHelp(argv[0]);
             return 0;
         }
-        if (arg == "--version" || arg == "-v")
+        else if (arg == "--version" || arg == "-v")
         {
             printVersion(argv[0]);
             return 0;
         }
-        if (arg == "--secret" || arg == "-s")
+        else if (arg == "--port" || arg == "-p")
+        {
+            if (i + 1 == argc)
+            {
+                std::cerr << arg << " required argument - port number.\n";
+                return 1;
+            }
+            port = atoi(argv[++i]);
+        }
+        else if (arg == "--secret" || arg == "-s")
         {
             if (i + 1 == argc)
             {
@@ -67,7 +80,7 @@ int main(int argc, char* argv[])
     try
     {
         boost::asio::io_service io_service;
-        Server server(io_service, secret, "/usr/share/freeradius/dictionary");
+        Server server(io_service, secret, port, "/usr/share/freeradius/dictionary");
         io_service.run();
     }
     catch (const std::exception& e)
