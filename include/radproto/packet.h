a73f7d45ca1b948f7a8b0b80fc0de8174f08a28f
Elena Mamontova <helenh463@gmail.com>
2024-02-15 16:05:15 +0200

    Parameter m_recvBuffer is changed to buffer in the constructor Packet declaration.

--- include/radproto/packet.h
+++ include/radproto/packet.h
@@ -13,7 +13,7 @@ namespace RadProto
     class Packet
     {
         public:
-            Packet(const std::array<uint8_t, 4096>& m_recvBuffer, size_t bytes, const std::string& secret);
+            Packet(const std::array<uint8_t, 4096>& buffer, size_t bytes, const std::string& secret);
             Packet(uint8_t type, uint8_t id, const std::array<uint8_t, 16>& auth, const std::vector<Attribute*>& attributes, const std::vector<VendorSpecific*>& vendorSpecific);
             Packet(const Packet& other);
             ~Packet();
