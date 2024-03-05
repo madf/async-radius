605e1af08139cafe692c384ba855f0de36c14ca0
Elena Mamontova <helenh463@gmail.com>
2024-03-05 14:36:30 +0200

    The method attributeCode call added in the makeResponse method for "NAS-Port", "NAS-IP-Address", "User-Password", "Callback-Number", "Chap-Password".

--- sample/server.cpp
+++ sample/server.cpp
@@ -21,14 +21,14 @@ RadProto::Packet Server::makeResponse(const RadProto::Packet& request)
 {
     std::vector<RadProto::Attribute*> attributes;
     attributes.push_back(new RadProto::String(m_dictionaries.attributeCode("User-Name"), "test"));
-    attributes.push_back(new RadProto::Integer(m_dictionaries.attributes().code("NAS-Port"), 20));
+    attributes.push_back(new RadProto::Integer(m_dictionaries.attributeCode("NAS-Port"), 20));
     std::array<uint8_t, 4> address {127, 104, 22, 17};
-    attributes.push_back(new RadProto::IpAddress(m_dictionaries.attributes().code("NAS-IP-Address"), address));
-    attributes.push_back(new RadProto::Encrypted(m_dictionaries.attributes().code("User-Password"), "password123"));
+    attributes.push_back(new RadProto::IpAddress(m_dictionaries.attributeCode("NAS-IP-Address"), address));
+    attributes.push_back(new RadProto::Encrypted(m_dictionaries.attributeCode("User-Password"), "password123"));
     std::vector<uint8_t> bytes {'1', '2', '3', 'a', 'b', 'c'};
-    attributes.push_back(new RadProto::Bytes(m_dictionaries.attributes().code("Callback-Number"), bytes));
+    attributes.push_back(new RadProto::Bytes(m_dictionaries.attributeCode("Callback-Number"), bytes));
     std::vector<uint8_t> chapPassword {'1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g' };
-    attributes.push_back(new RadProto::ChapPassword(m_dictionaries.attributes().code("CHAP-Password"), 1, chapPassword));
+    attributes.push_back(new RadProto::ChapPassword(m_dictionaries.attributeCode("CHAP-Password"), 1, chapPassword));
 
     std::vector<RadProto::VendorSpecific> vendorSpecific;
     std::vector<uint8_t> vendorValue {'0', '0', '0', '3'};
