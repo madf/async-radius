57144fadc7818df5590222c498f760a3e011f6b4
Elena Mamontova <helenh463@gmail.com>
2024-04-12 16:02:40 +0300

    The name RadProtoErrorTests added to BOOST_AUTO_TEST_SUITE().

--- tests/error_tests.cpp
+++ tests/error_tests.cpp
@@ -10,7 +10,7 @@
 #include <boost/test/unit_test.hpp>
 #pragma GCC diagnostic pop
 
-BOOST_AUTO_TEST_SUITE()
+BOOST_AUTO_TEST_SUITE(RadProtoErrorTests)
 
 BOOST_AUTO_TEST_CASE(ErrorCodeConstruction)
 {
