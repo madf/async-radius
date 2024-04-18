8b6dd889375abe4aaf627d980616f39902b84377
Elena Mamontova <helenh463@gmail.com>
2024-04-18 16:33:45 +0300

    IntegerDataConstructor, IntegerValueConstructor, IntegerClone test cases added.

--- tests/attribute_tests.cpp
+++ tests/attribute_tests.cpp
@@ -57,4 +57,48 @@ BOOST_AUTO_TEST_CASE(StringClone)
     BOOST_CHECK_EQUAL(cs->type(), 1);
 }
 
+BOOST_AUTO_TEST_CASE(IntegerDataConstructor)
+{
+    std::vector<uint8_t> d {0, 0, 0, 20};
+    RadProto::Integer s(5, d.data(), d.size());
+
+    BOOST_CHECK_EQUAL(s.toString(), "20");
+
+    std::vector<uint8_t> values = s.toVector({}, {});
+    std::vector<uint8_t> expected {5, 6, 0, 0, 0, 20};
+
+    BOOST_TEST(values == expected, boost::test_tools::per_element());
+
+    BOOST_CHECK_EQUAL(s.type(), 5);
+}
+
+BOOST_AUTO_TEST_CASE(IntegerValueConstructor)
+{
+    RadProto::Integer v(5, 20);
+
+    BOOST_CHECK_EQUAL(v.toString(), "20");
+
+    std::vector<uint8_t> values = v.toVector({}, {});
+    std::vector<uint8_t> expected {5, 6, 0, 0, 0, 20};
+
+    BOOST_TEST(values == expected, boost::test_tools::per_element());
+
+    BOOST_CHECK_EQUAL(v.type(), 5);
+}
+
+BOOST_AUTO_TEST_CASE(IntegerClone)
+{
+    RadProto::Integer c(5, 20);
+    std::unique_ptr<RadProto::Attribute> cs(c.clone());
+
+    BOOST_CHECK_EQUAL(cs->toString(), "20");
+
+    std::vector<uint8_t> values = cs->toVector({}, {});
+    std::vector<uint8_t> expected {5, 6, 0, 0, 0, 20};
+
+    BOOST_TEST(values == expected, boost::test_tools::per_element());
+
+    BOOST_CHECK_EQUAL(cs->type(), 5);
+}
+
 BOOST_AUTO_TEST_SUITE_END()
