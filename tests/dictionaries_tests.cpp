4d7a933fa5d39a6bf71c1f9469b6499397e7b2a4
Elena Mamontova <helenh463@gmail.com>
2024-08-15 16:35:42 +0300

    Method call append() fixed, object BasicDictionary c added in test case BasicDictionary. Test case FunctionAdd added.

--- tests/dictionaries_tests.cpp
+++ tests/dictionaries_tests.cpp
@@ -16,6 +16,25 @@
 
 BOOST_AUTO_TEST_SUITE(DictionariesTests)
 
+BOOST_AUTO_TEST_CASE(FunctionAdd)
+{
+    RadProto::BasicDictionary b;
+
+    b.add(1, "User-Name");
+    b.add(1, "User");
+    b.add(2, "abc");
+    b.add(3, "abc");
+
+    BOOST_CHECK_EQUAL(b.name(1), "User");
+    BOOST_CHECK_EQUAL(b.name(2), "abc");
+    BOOST_CHECK_EQUAL(b.code("User-Name"), 1);
+    BOOST_CHECK_EQUAL(b.code("abc"), 3);
+
+    BOOST_CHECK_THROW(b.name(3), std::out_of_range);
+
+    BOOST_CHECK_THROW(b.code("User"), std::out_of_range);
+}
+
 BOOST_AUTO_TEST_CASE(BasicDictionary)
 {
     RadProto::BasicDictionary b;
@@ -25,16 +44,34 @@ BOOST_AUTO_TEST_CASE(BasicDictionary)
     BOOST_CHECK_THROW(b.code(""), std::out_of_range);
 
     b.add(1, "User-Name");
+    b.add(1, "User");
     b.add(2, "abc");
-    b.add(2, "def");
-    b.append(b);
 
-    BOOST_CHECK_EQUAL(b.name(1), "User-Name");
+    BOOST_CHECK_EQUAL(b.name(1), "User");
     BOOST_CHECK_EQUAL(b.name(2), "abc");
-
     BOOST_CHECK_EQUAL(b.code("User-Name"), 1);
     BOOST_CHECK_EQUAL(b.code("abc"), 2);
-    BOOST_CHECK_EQUAL(b.code("def"), 2);
+
+    BOOST_CHECK_THROW(b.code("User"), std::out_of_range);
+
+    RadProto::BasicDictionary c;
+    c.add(2, "def");
+    c.add(3, "ghi");
+
+    BOOST_CHECK_EQUAL(c.name(2), "def");
+    BOOST_CHECK_EQUAL(c.name(3), "ghi");
+    BOOST_CHECK_EQUAL(c.code("def"), 2);
+    BOOST_CHECK_EQUAL(c.code("ghi"), 3);
+
+    c.append(b);
+
+    BOOST_CHECK_EQUAL(c.name(1), "User");
+    BOOST_CHECK_EQUAL(c.name(2), "abc");
+    BOOST_CHECK_EQUAL(c.name(3), "ghi");
+    BOOST_CHECK_EQUAL(c.code("User-Name"), 1);
+    BOOST_CHECK_EQUAL(c.code("abc"), 2);
+    BOOST_CHECK_EQUAL(c.code("def"), 2);
+    BOOST_CHECK_EQUAL(c.code("ghi"), 3);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
