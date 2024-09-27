3fb48db8ea70ce7f0440eef1334dccfa597ad59f
Elena Mamontova <helenh463@gmail.com>
2024-09-27 14:29:52 +0300

    The test case TestAttributeCode added.

--- tests/dictionaries_tests.cpp
+++ tests/dictionaries_tests.cpp
@@ -375,6 +375,13 @@ BOOST_AUTO_TEST_CASE(TestAttributeName)
     BOOST_CHECK_EQUAL(a.attributeName(1), "User-Name");
 }
 
+BOOST_AUTO_TEST_CASE(TestAttributeCode)
+{
+    RadProto::Dictionaries a("dictionary");
+
+    BOOST_CHECK_EQUAL(a.attributeCode("User-Password"), 2);
+}
+
 BOOST_AUTO_TEST_SUITE_END()
 
 BOOST_AUTO_TEST_SUITE_END()
