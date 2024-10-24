0f22deec96727d8b69a1bf6de7df5813d4510002
Elena Mamontova <helenh463@gmail.com>
2024-10-24 14:53:34 +0300

    The header files <array>, <vector> added. Test for error ec fixed in the checkSend and checkReceive functions. The Packet constructor changed to constructor for packet type equal 1 in the checkReceive function and test case for asyncSend function. The endpoint destination added to test case for asyncSend function. Parameter destination added to asyncSend function.

--- tests/socket_tests.cpp
+++ tests/socket_tests.cpp
@@ -6,6 +6,8 @@
 #include "radproto/vendor_attribute.h"
 #include "attribute_types.h"
 #include "utils.h"
+#include <array>
+#include <vector>
 #include <set>
 #include <string>
 #include <cstdint> //uint8_t, uint32_t
@@ -26,31 +28,23 @@ namespace
     void checkSend(const error_code& ec)
     {
         BOOST_CHECK(true);
-
-        if (ec)
-        {
-            BOOST_CHECK_EQUAL(ec.category().name(), "radproto");
-            BOOST_CHECK_EQUAL(ec.message(), "No error");
-        }
+        BOOST_REQUIRE(!ec);
     }
 
     void checkReceive(const error_code& ec, const std::optional<RadProto::Packet>& p)
     {
-        BOOST_CHECK_EQUAL(ec.category().name(), "radproto");
-        BOOST_CHECK_EQUAL(ec.message(), "No error");
+        BOOST_REQUIRE(!ec);
 
         BOOST_REQUIRE(p != std::nullopt);
 
         RadProto::Packet d = p.value();
-        BOOST_CHECK_EQUAL(d.type(), 2);
+        BOOST_CHECK_EQUAL(d.type(), 1);
 
         BOOST_CHECK_EQUAL(d.id(), 208);
 
         std::array<uint8_t, 16> auth {0x1a, 0x40, 0x43, 0xc6, 0x41, 0x0a, 0x08, 0x31, 0x12, 0x16, 0x80, 0x2c, 0x3e, 0x83, 0x12, 0x45};
 
         BOOST_TEST(d.auth() == auth, boost::test_tools::per_element());
-
-        const std::vector<RadProto::Attribute*> attributes {new RadProto::String(1, "test"), new RadProto::Integer(5, 20), new RadProto::IpAddress(4, {127, 104, 22, 17}), new RadProto::Bytes(19, {'1', '2', '3', 'a', 'b', 'c'}), new RadProto::ChapPassword(3, 1, {'1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g' })};
 
         const auto& attrs = d.attributes();
 
@@ -60,29 +54,29 @@ namespace
         BOOST_CHECK_EQUAL(attr0->type(), RadProto::USER_NAME);
         BOOST_CHECK_EQUAL(attr0->toString(), "test");
 
-        auto* attr1 = findAttribute(attrs, RadProto::NAS_PORT);
+        auto* attr1 = findAttribute(attrs, RadProto::USER_PASSWORD);
 
         BOOST_REQUIRE(attr1 != nullptr);
-        BOOST_CHECK_EQUAL(attr1->type(), RadProto::NAS_PORT);
-        BOOST_CHECK_EQUAL(attr1->toString(), "20");
+        BOOST_CHECK_EQUAL(attr1->type(), RadProto::USER_PASSWORD);
+        BOOST_CHECK_EQUAL(attr1->toString(), "123456");
 
         auto* attr2 = findAttribute(attrs, RadProto::NAS_IP_ADDRESS);
 
         BOOST_REQUIRE(attr2 != nullptr);
         BOOST_CHECK_EQUAL(attr2->type(), RadProto::NAS_IP_ADDRESS);
-        BOOST_CHECK_EQUAL(attr2->toString(), "127.104.22.17");
+        BOOST_CHECK_EQUAL(attr2->toString(), "127.0.0.1");
 
-        auto* attr3 = findAttribute(attrs, RadProto::CALLBACK_NUMBER);
+        auto* attr3 = findAttribute(attrs, RadProto::NAS_PORT);
 
         BOOST_REQUIRE(attr3 != nullptr);
-        BOOST_CHECK_EQUAL(attr3->type(), RadProto::CALLBACK_NUMBER);
-        BOOST_CHECK_EQUAL(attr3->toString(), "313233616263");
+        BOOST_CHECK_EQUAL(attr3->type(), RadProto::NAS_PORT);
+        BOOST_CHECK_EQUAL(attr3->toString(), "1");
 
-        auto* attr4 = findAttribute(attrs, RadProto::CHAP_PASSWORD);
+        auto* attr4 = findAttribute(attrs, RadProto::MESSAGE_AUTHENTICATOR);
 
         BOOST_REQUIRE(attr4 != nullptr);
-        BOOST_CHECK_EQUAL(attr4->type(), RadProto::CHAP_PASSWORD);
-        BOOST_CHECK_EQUAL(attr4->toString(), "1 31323334353637383961626364656667");
+        BOOST_CHECK_EQUAL(attr4->type(), RadProto::MESSAGE_AUTHENTICATOR);
+        BOOST_CHECK_EQUAL(attr4->toString(), "F3E000E77DEB51EB815D52373D06B71B");
 
         auto* attr5 = findAttribute(attrs, RadProto::FRAMED_PROTOCOL);
 
@@ -113,9 +107,9 @@ BOOST_AUTO_TEST_SUITE(SocketTests)
 
 BOOST_AUTO_TEST_CASE(TestConstructor)
 {
-boost::asio::io_service io_service;
+    boost::asio::io_service io_service;
 
-BOOST_CHECK_NO_THROW(RadProto::Socket s(io_service, "secret", 3000));
+    BOOST_CHECK_NO_THROW(RadProto::Socket s(io_service, "secret", 3000));
 
 }
 
@@ -123,17 +117,19 @@ BOOST_AUTO_TEST_CASE(TestAsyncSend)
 {
     std::array<uint8_t, 16> auth {0x1a, 0x40, 0x43, 0xc6, 0x41, 0x0a, 0x08, 0x31, 0x12, 0x16, 0x80, 0x2c, 0x3e, 0x83, 0x12, 0x45};
 
-    const std::vector<RadProto::Attribute*> attributes {new RadProto::String(1, "test"), new RadProto::Integer(5, 20), new RadProto::IpAddress(4, {127, 104, 22, 17}), new RadProto::Bytes(19, {'1', '2', '3', 'a', 'b', 'c'}), new RadProto::ChapPassword(3, 1, {'1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g' })};
+    const std::vector<RadProto::Attribute*> attributes {new RadProto::String(1, "test"), new RadProto::Encrypted(2, "123456"), new RadProto::IpAddress(4, {127, 0, 0, 1}), new RadProto::Integer(5, 1), new RadProto::Bytes(80, {0xf3, 0xe0, 0x00, 0xe7, 0x7d, 0xeb, 0x51, 0xeb, 0x81, 0x5d, 0x52, 0x37, 0x3d, 0x06, 0xb7, 0x1b}), new RadProto::Integer(7, 1)};
 
     std::vector<RadProto::VendorSpecific> vendorSpecific;
     vendorSpecific.push_back(RadProto::VendorSpecific(171, 1, {0, 0, 0, 3}));
 
-    RadProto::Packet p(2, 208, auth, attributes, vendorSpecific);
+    RadProto::Packet p(1, 208, auth, attributes, vendorSpecific);
 
     boost::asio::io_service io_service;
 
+    boost::asio::ip::udp::endpoint destination(boost::asio::ip::address_v4::from_string("127.0.0.1"), 3000);
+
     RadProto::Socket s(io_service, "secret", 3000);
-    s.asyncSend(p, checkSend);
+    s.asyncSend(p, destination, checkSend);
 
     s.asyncReceive(checkReceive);
 
