b46802ac0a30a8a01054d448ceba0a4a5af210fe
Elena Mamontova <helenh463@gmail.com>
2025-10-03 15:55:38 +0300

    Parameter type changed to code. Function type() changed to code(). Class memeber m_type changed to m_code.

--- include/radproto/packet.h
+++ include/radproto/packet.h
@@ -13,10 +13,10 @@ namespace RadProto
     {
         public:
             Packet(const uint8_t* buffer, size_t size, const std::string& secret);
-            Packet(uint8_t type, uint8_t id, const std::array<uint8_t, 16>& auth, const std::vector<Attribute*>& attributes, const std::vector<VendorSpecific>& vendorSpecific);
+            Packet(uint8_t code, uint8_t id, const std::array<uint8_t, 16>& auth, const std::vector<Attribute*>& attributes, const std::vector<VendorSpecific>& vendorSpecific);
             Packet(const Packet& other);
             ~Packet();
-            uint8_t type() const { return m_type; }
+            uint8_t code() const { return m_code; }
             uint8_t id() const { return m_id; };
             const std::array<uint8_t, 16>& auth() const { return m_auth; }
             const std::vector<Attribute*>& attributes() const { return m_attributes; }
@@ -24,7 +24,7 @@ namespace RadProto
             const std::vector<uint8_t> makeSendBuffer(const std::string& secret) const;
 
         private:
-            uint8_t m_type;
+            uint8_t m_code;
             uint8_t m_id;
             bool m_recalcAuth;
             std::array<uint8_t, 16> m_auth;
