5532d57f2d3fb241b8eba2e5aed87a12ebf8a135
Elena Mamontova <helenh463@gmail.com>
2025-10-03 17:30:34 +0300

    Parameter type changed to code. Function type() changed to code().

--- tests/utils.cpp
+++ tests/utils.cpp
@@ -1,10 +1,10 @@
 #include "utils.h"
 
-RadProto::Attribute* findAttribute(const std::vector<RadProto::Attribute*>& attributes, RadProto::Attribute_Types type)
+RadProto::Attribute* findAttribute(const std::vector<RadProto::Attribute*>& attributes, RadProto::Attribute_Codes code)
 {
     for (const auto& b : attributes)
     {
-        if (b->type() == type)
+        if (b->code() == code)
             return b;
     }
     return nullptr;
