dd5fb8787198bfa249b337a76907eb85a6580ce3
Elena Mamontova <helenh463@gmail.com>
2025-10-03 16:06:43 +0300

    Parameter vendorType changed to vendorAttributeCode. Function vendorType() changed to vendorAttributeCode(). Class memeber m_vendorType changed to m_vendorAttributeCode.

--- include/radproto/vendor_attribute.h
+++ include/radproto/vendor_attribute.h
@@ -10,14 +10,14 @@ namespace RadProto
     {
         public:
             VendorSpecific(const uint8_t* data);
-            VendorSpecific(uint32_t vendorId, uint8_t vendorType, const std::vector<uint8_t>& vendorValue);
+            VendorSpecific(uint32_t vendorId, uint8_t vendorAttributeCode, const std::vector<uint8_t>& vendorValue);
             std::string toString() const;
-            uint8_t vendorType() const { return m_vendorType; }
+            uint8_t vendorAttributeCode() const { return m_vendorAttributeCode; }
             uint32_t vendorId() const { return m_vendorId; }
             std::vector<uint8_t> toVector() const;
         private:
             uint32_t m_vendorId;
-            uint8_t m_vendorType;
+            uint8_t m_vendorAttributeCode;
             std::vector<uint8_t> m_value;
     };
 }
