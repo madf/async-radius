8d6e68f3052783d340dcd5033112273fe9f35209
Elena Mamontova <helenh463@gmail.com>
2025-12-22 15:31:26 +0200

    The invalidValueTypeMember changed to invalidValueType in enum class Error.

--- include/radproto/error.h
+++ include/radproto/error.h
@@ -22,7 +22,7 @@ namespace RadProto
         invalidAttributeCode,
         invalidAttributeSize,
         invalidAttributeType,
-        invalidValueTypeMember,
+        invalidValueType,
         invalidVendorSpecificAttributeId,
         suchAttributeNameAlreadyExists,
         suchAttributeCodeAlreadyExists,
